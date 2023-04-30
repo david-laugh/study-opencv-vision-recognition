f5120f51042186bca6e7846bfc8d01affe34cb6b
Lee Gyu Hyuk <matda8080@gmail.com>
2023-05-01 08:11:45 +0900

    Update Weeks

---
+++ main.py
@@ -0,0 +1,60 @@
+
+# # print()
+# print("Hello, World", end="\n")
+
+# # input()
+# def myInput(text):
+#     print(text, end="")
+#     userInput = input()
+#     return userInput
+
+# def x(a=1, b=3):
+#     return a + b
+
+# print(x())
+# print(x(a=2))
+# print(x(b=5))
+
+# a = input("input : ") # string
+# b = myInput("input : ")
+# print(a)
+# print(b)
+
+# # len()
+# print(len("string"))
+# print(len([1, 2, 3]))
+
+# def myLen(data):
+#     count = 0
+#     for _ in data:
+#         count += 1
+#     return count
+# print(myLen("string"))
+
+# str()
+a = 56
+print(type(a))
+a = str(a)
+print(type(a))
+
+a = 56.535
+a = round(a, 1)
+print(a)
+
+print(list("abc"))
+# float()
+
+# string "hi"
+# int 1 
+# float 1.4
+# list [1, {"1": 1}, [1, 2, 3], "strign"]
+# dict = {
+#  key: value
+# }
+# tuple = (1, 2)
+# nonetype
+# set
+# tuple = (1, 2, 5)
+# print(tuple[1])
+# for i in range(10, 1, -1):
+#     print(i)
---
+++ week01/summary.md
@@ -0,0 +1,20 @@
+# Week 01. Google Colab 사용법
+
+1. Python?
+    - 개요
+    - 다운로드 및 설치
+    - 통합 개발 환경 (IDE)
+        - PyCharm, VScode
+
+2. Jupyter Notebook
+
+3. Google Colab
+    - Google Drive + Jupyter Notebook
+        - 구글 계정 전용의 가상 머신 지원
+        - Google drive 문서와 같이 링크만으로 접근 / 협업 가능
+        - 코드 실행 시 딜레이 발생
+    - Google Colab 사용법
+        - ShortCut
+            - 코드 셀 시행 : Ctrl + Enter
+            - 코드 셀 실행 후 다음 셀 이동 : Shift + Enter
+        
